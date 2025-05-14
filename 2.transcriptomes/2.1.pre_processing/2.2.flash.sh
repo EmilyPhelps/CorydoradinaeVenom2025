85f669ca10329860a53d86e703f72e1936c405c4
Emily Phelps <52965134+EmilyPhelps@users.noreply.github.com>
2025-05-14 09:35:32 +0100

    Create 2.2.flash.sh

---
+++ 2.transcriptomes/2.1.pre_processing/2.2.flash.sh
@@ -0,0 +1,63 @@
+Help()
+{
+   # Display Help
+   echo "This script is written to assemble to transcriptomes. This is written for the HPC (UEA ADA 2024)"
+   echo "Change the dependencies if need be."
+   echo "Pre-processing and assembling pipeline"
+   echo "   -----------------"
+   echo "   | Combining reads |"
+   echo "   -----------------"
+    echo "          ⤵         "
+   echo "   -----------------"
+   echo "   | Trim Adapters |"
+   echo "   -----------------"
+   echo "          ⤵         "
+   echo "   -----------------"
+   echo "   | Map to transcritpme |"
+   echo "   -----------------"
+   echo "          ⤵        "
+   echo "-------------------------"
+   echo "| Quantify expression    |"
+   echo "-------------------------"
+   echo "     ✧*｡٩(ˊᗜˋ*)و✧*｡"
+   echo
+   echo "Syntax: "
+   echo 
+   echo options
+   echo "h     Print this Help."
+   echo "s     Sample list, with path"
+   echo "o     output directory"
+   echo ""
+}
+
+###############################################  Options ################################################
+
+# Add options in by adding another letter eg. :x, and then include a letter and some commands.
+# Semi colon placement mega important if you want to include your own input value.
+while getopts ":hs:o:" option; do
+   case ${option} in
+      h) # display Help
+         Help
+         exit;;
+       s) LIST=$OPTARG;;
+       o) OUTDIR=$OPTARG;;
+      \?) # incorrect option
+         echo "Error: Invalid option щ(ಥДಥщ)"
+         exit;;
+   esac
+done
+
+if [ -z ${LIST} ] || [ -z $OUTDIR} ]; then
+   echo "Missing options"
+   exit 1
+fi
+
+if [[ "${INDEX}" -ne 1 ]]; then
+   INDEX=0
+fi
+###################################### Dependencies and Version. ###########################################
+
+PATH=flash/:$PATH
+
+################################################## Program ####################################################
+
