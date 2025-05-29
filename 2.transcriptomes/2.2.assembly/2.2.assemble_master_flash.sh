3c538df85520e2d26df954913fe1f36881e15353
Emily Phelps <52965134+EmilyPhelps@users.noreply.github.com>
2025-05-29 09:28:36 +0100

    Update 2.2.assemble_master_flash.sh

--- 2.transcriptomes/2.2.assembly/2.2.assemble_master_flash.sh
+++ 2.transcriptomes/2.2.assembly/2.2.assemble_master_flash.sh
@@ -88,8 +88,8 @@ for sp in `cat $species_list`; do
   for sample in `echo ${sample_list}`; do 
      
   #   cat ${DIR}/$sample*fastq > ${OUTDIR}/${sp}/int_concat/${sample}_concat.fastq
-     R1=${DIR}/${sample}*_notCombined_1.fastq
-     R2=${DIR}/${sample}*_notCombined_2.fastq
+     R1=${DIR}/${sample}*notCombined_1.fastq
+     R2=${DIR}/${sample}*notCombined_2.fastq
      single=${DIR}/${sample}*extendedFrags.fastq
 
       echo -e "${sp}\t${sample}\t${R1}\t${R2}" >> ${OUTDIR}/${sp}_trin.txt
