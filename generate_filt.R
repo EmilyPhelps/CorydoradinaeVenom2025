ebc7bcbb288dde05430f55e3e73cdae9aa5f179d
Emily Phelps <52965134+EmilyPhelps@users.noreply.github.com>
2025-07-25 12:11:56 +0100

    Create generate_filt.R

---
+++ generate_filt.R
@@ -0,0 +1,19 @@
+#Rscript
+library(tidyverse)
+
+args = commandArgs(trailingOnly=TRUE)
+
+rep <-  read_tsv(args[1], col_names=FALSE) 
+colnames(rep) <- c("gene", "TE", "X", "Seq", "Y")
+
+max_fields <- max(str_count(rep$Seq, ";")) + 1
+
+rep %>%
+  separate(Seq, into = paste0("S", 1:max_fields), sep = ";", fill = "right") %>%
+  pivot_longer(
+    cols = starts_with("S"),
+    names_to = "Scol",
+    values_to = "Transcripts"
+  ) %>%
+mutate(Transcripts=gsub("\\.", "_", trimws(gsub("_.*", "", Transcripts)))) %>% 
+dplyr::select(Transcripts) %>% write_tsv(paste0(args[1], "output.tsv")
