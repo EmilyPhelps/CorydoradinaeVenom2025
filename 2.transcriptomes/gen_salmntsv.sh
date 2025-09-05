da9b94a143312299858c21b48a5730038a6c5d4a
Emily Phelps <52965134+EmilyPhelps@users.noreply.github.com>
2025-09-05 14:09:17 +0100

    Create gen_salmntsv.sh

---
+++ 2.transcriptomes/gen_salmntsv.sh
@@ -0,0 +1,11 @@
+#Quick code to create the files for inputting to salmon in trinity
+dir= #path with the trimmed fq files
+id= # the prefix, e.g. SIM NAR TUK
+output=./
+   
+mapfile -t f1 < <(find "${dir}" -name "${id}*_1.fastq*" | sort)
+mapfile -t f2 < <(find "${dir}" -name "${id}*_2.fastq*" | sort)
+
+for ((i=0; i<${#f1[@]}; i++)); do
+  echo -e "${f1[i]}\t${f2[i]}\t" >> ${output}/${id}_fq.tsv
+done
